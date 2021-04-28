fcb0ab6d2775a11dfcbff5e5d5b9b34f98410773
eduardo fuentes <eduardo.fuentesreyes@gmail.com>
2021-04-28 11:34:05 -0400

    moviesSearch

---
+++ src/pages/notFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ButtonBackToHome } from '../components/buttonBackToHome'
+
+export const NotFound = () => (
+    <div>
+        <h1 className='title'>404!</h1>
+        <h2 className='subtitle'>This page not exist</h2>
+        <ButtonBackToHome />
+    </div>
+    
+)
